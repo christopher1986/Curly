0570df574b644076f1200bccc4c5ced011acaa4f
Chris Harris <c.harris@hotmail.com>
2015-09-11 13:15:19 +0200

    This class now supports the Comparable and Equatable interface and will them to sort arrays

--- src/Util/Arrays.php
+++ src/Util/Arrays.php
@@ -2,7 +2,8 @@
 
 namespace Curly\Util;
 
-use Curly\Collection\Comparator\Comparator;
+use Curly\Common\ComparatorInterface;
+use Curly\Common\EquatableInterface;
 
 /**
  *
@@ -13,13 +14,35 @@ use Curly\Collection\Comparator\Comparator;
 class Arrays
 {
     /**
+     * Returns true if the specified array contains the specified value.
+     *
+     * @param array $arr the array to be searched.
+     * @param mixed $value the value to find.
+     * @return bool true if the specified array contains the specified value, false otherwise.
+     */
+    public static function contains(array $arr, $value)
+    {
+        if ($value instanceof EquatableInterface) {
+            foreach ($arr as $element) { 
+                if ($value->equals($element)) {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+        
+        return in_array($arr, $value, true);
+    }
+
+    /**
      * Sorts the specified array according to the order imposed by the specified {@link Comparator}.
      *
      * @param array $arr the array to be sorted.
-     * @param Comparator $comparator a comparator that will determine the order of the array.
+     * @param ComparatorInterface $comparator a comparator that will determine the order of the array.
      * @param bool $associative (optional) if true index association will be maintained.
      */
-    public static function sort(array &$arr, Comparator $comparator, $associative = false)
+    public static function sort(array &$arr, ComparatorInterface $comparator, $associative = false)
     {    
         if ($associative) {
             uasort($arr, array($comparator, 'compare'));
