ed8c33587c3f05bd49d311e56aa296e63a8ad00d
Chris Harris <c.harris@hotmail.com>
2015-09-11 13:14:31 +0200

    Changes related to the Lexer which now returns a Stream of tokens and some documentation changes.

--- src/SubparserInterface.php
+++ src/SubparserInterface.php
@@ -2,26 +2,27 @@
 
 namespace Curly;
 
-use SplStack;
+use Curly\Collection\Stream\TokenStream;
+use Curly\Parser\TokenInterface;
 
 /**
- * The SubparserInterface parses a collection of tokens into a node which is part of an abstract syntax tree. 
- * 
- * Because classes implementing this interface operate on a collection of tokens and not on an input string
- * it's only possible to use a subparser after the lexical analysis has taken place or to make it part of a 
- * larger parsing process where the input string has been tokenized by a ParserInterface instance.
+ * The SubparserInterface parses a stream of tokens into one or more nodes which are part of an abstract syntax tree.
+ *
+ * A subparser only creates nodes from a small portion of the token stream. A parser may temporarily delegate the
+ * parsing process to a subparser which is capable of parsing a specific collection of tokens.
  *
  * @author Chris Harris
  * @version 1.0.0
  * @since 1.0.0
  */
 interface SubparserInterface
-{
+{    
     /**
-     * Creates part of the abstract syntax tree by parsing a last-in-first-out (LIFO) stack
-     * from a {@link LexerInterface} instance.
+     * Creates part of the abstract syntax tree by parsing a collection of tokens.
      *
-     * @param SplStack $tokens collection of tokens.
+     * @param ParserInterface $parser the template parser.
+     * @param TokenStream the stream of tokens to parse.
+     * @return NodeInterface|null a node or null if not enough tokens have been parsed yet.
      */
-    public function parse(SplStack $tokens);
+    public function parse(ParserInterface $parser, TokenStream $stream);
 }
