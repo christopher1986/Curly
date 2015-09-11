3f210d591f9249d1491746787b3c97a6563cffeb
Chris Harris <c.harris@hotmail.com>
2015-09-11 13:19:54 +0200

    The ContextInterface is rendered with a Template object and provides access to variables values.

---
+++ src/ContextInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Curly;
+
+/**
+ * The ContextInterface is rendered with a Template object and provides access
+ * to variables values. 
+ *
+ * A template context is similar to a dictionary or map in most other programming languages
+ * and creates a mapping between variable names and variable values which are then made
+ * accessible to the template.
+ *
+ * @author Chris Harris
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+interface ContextInterface
+{
+
+}
