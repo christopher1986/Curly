d5920c7a26b2e56737e04901091281e573c26717
Chris Harris <c.harris@hotmail.com>
2015-09-12 22:15:59 +0200

    Moved the operator to another folder and changed the namespace to reflect this change.

--- src/Lang/Operator/Binary/LessOperator.php
+++ src/Lang/Operator/Binary/LessOperator.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Curly\Lang\Operator\Binary\Comparison;
+namespace Curly\Lang\Operator\Binary;
 
 use Curly\Ast\NodeInterface;
-use Curly\Ast\Node\Expression\BinaryNode;
+use Curly\Ast\Node\Expression\Binary\LessNode;
 use Curly\Lang\Operator\AbstractBinaryOperator;
 
 /** 
@@ -36,7 +36,7 @@ class LessOperator extends AbstractBinaryOperator
      */
     public function createNode(NodeInterface $left, NodeInterface $right, $lineNumber = -1, $flags = 0x00)
     {
-        return new BinaryNode($left, $right, $lineNumber, $flags);
+        return new LessNode($left, $right, $lineNumber, $flags);
     }
 }
 
