9dd0e43fd604777ef134a0a5ee89221dc961f490
Chris Harris <c.harris@hotmail.com>
2015-09-12 22:14:33 +0200

    Moved the operator to another folder and changed the namespace to reflect this change.

--- src/Lang/Operator/Binary/GreaterEqualOperator.php
+++ src/Lang/Operator/Binary/GreaterEqualOperator.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Curly\Lang\Operator\Binary\Comparison;
+namespace Curly\Lang\Operator\Binary;
 
 use Curly\Ast\NodeInterface;
-use Curly\Ast\Node\Expression\BinaryNode;
+use Curly\Ast\Node\Expression\Binary\GreaterEqualNode;
 use Curly\Lang\Operator\AbstractBinaryOperator;
 
 /** 
@@ -36,7 +36,7 @@ class GreaterEqualOperator extends AbstractBinaryOperator
      */
     public function createNode(NodeInterface $left, NodeInterface $right, $lineNumber = -1, $flags = 0x00)
     {
-        return new BinaryNode($left, $right, $lineNumber, $flags);
+        return new GreaterEqualNode($left, $right, $lineNumber, $flags);
     }
 }
 
