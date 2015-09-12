0df209b3b23c1da2ef51ceea0324738ea3678dc1
Chris Harris <c.harris@hotmail.com>
2015-09-12 22:14:02 +0200

    Moved the operator to another folder and changed the namespace to reflect this change.

--- src/Lang/Operator/Binary/EqualOperator.php
+++ src/Lang/Operator/Binary/EqualOperator.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Curly\Lang\Operator\Binary\Comparison;
+namespace Curly\Lang\Operator\Binary;
 
 use Curly\Ast\NodeInterface;
-use Curly\Ast\Node\Expression\BinaryNode;
+use Curly\Ast\Node\Expression\Binary\EqualNode;
 use Curly\Lang\Operator\AbstractBinaryOperator;
 
 /** 
@@ -36,7 +36,7 @@ class EqualOperator extends AbstractBinaryOperator
      */
     public function createNode(NodeInterface $left, NodeInterface $right, $lineNumber = -1, $flags = 0x00)
     {
-        return new BinaryNode($left, $right, $lineNumber, $flags);
+        return new EqualNode($left, $right, $lineNumber, $flags);
     }
 }
 
