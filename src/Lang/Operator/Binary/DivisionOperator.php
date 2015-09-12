beb259d510c67089359d20add0b764b57299e3af
Chris Harris <c.harris@hotmail.com>
2015-09-12 22:12:17 +0200

    Moved the operator to another folder and changed the namespace to reflect this change.

--- src/Lang/Operator/Binary/DivisionOperator.php
+++ src/Lang/Operator/Binary/DivisionOperator.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Curly\Lang\Operator\Binary\Arithmetic;
+namespace Curly\Lang\Operator\Binary;
 
 use Curly\Ast\NodeInterface;
-use Curly\Ast\Node\Expression\BinaryNode;
+use Curly\Ast\Node\Expression\Binary\DivisionNode;
 use Curly\Lang\Operator\AbstractBinaryOperator;
 
 /** 
@@ -36,6 +36,6 @@ class DivisionOperator extends AbstractBinaryOperator
      */
     public function createNode(NodeInterface $left, NodeInterface $right, $lineNumber = -1, $flags = 0x00)
     {
-        return new BinaryNode($left, $right, $lineNumber, $flags);
+        return new DivisionNode($left, $right, $lineNumber, $flags);
     }
 }
