18e7dfe936d099900521872bfe6a9de09ffa9e7e
Chris Harris <c.harris@hotmail.com>
2015-09-14 15:34:41 +0200

    Some minor changes to use statements and the matches and expects methods of the stream used by this tag.

--- src/Lang/Tag/IfTag.php
+++ src/Lang/Tag/IfTag.php
@@ -2,7 +2,10 @@
 
 namespace Curly\Lang\Tag;
 
+use Curly\Ast\Node\ConditionalNode;
+use Curly\Ast\Node\IfNode;
 use Curly\Collection\Stream\TokenStream;
+use Curly\Lang\TagInterface;
 use Curly\ParserInterface;
 use Curly\Parser\Token;
 
@@ -13,38 +16,77 @@ use Curly\Parser\Token;
  * @version 1.0.0
  * @since 1.0.0
  */
-class IfTag extends AbstractTag
+class IfTag implements TagInterface
 {    
     /**
      * {@inheritDoc}
      */
-    public function getTags()
+    public function getTag()
     {
-        return array('if', 'elseif', 'else', 'endif');
+        return 'if';
     }
         
     /**
      * {@inheritDoc}
      */
     public function parse(ParserInterface $parser, TokenStream $stream)
+    {    
+        $startToken = $stream->current();
+        $conditions = $this->parseConditions($parser, $stream);
+        
+        if ($stream->matches(sprintf('%s:else', Token::T_IDENTIFIER))) {
+            $token = $stream->current();
+            
+            $stream->consume();
+            $stream->expects(Token::T_COLON);
+            
+            $children = $parser->parse($stream, sprintf('%s:endif', Token::T_IDENTIFIER));
+            $conditions[] = new ConditionalNode(null, $children, $token->getLineNumber());
+        }
+        
+        $stream->expects(sprintf('%s:endif', Token::T_IDENTIFIER));
+        $stream->expects(Token::T_SEMICOLON, Token::T_CLOSE_TAG);
+        
+        return new IfNode($conditions, $startToken->getLineNumber());
+    }
+    
+    /**
+     * Returns a collection of {@link ConditionalNode} instances.
+     *
+     * @param ParserInterface $parser the template parser.
+     * @param TokenStream the stream of tokens to parse.
+     * @return array a collection of {@link ConditionalNode} instances.
+     */
+    private function parseConditions(ParserInterface $parser, TokenStream $stream)
     {
-        $token = $stream->current();
+        $types = array(
+            sprintf('%s:else', Token::T_IDENTIFIER), 
+            sprintf('%s:endif', Token::T_IDENTIFIER), 
+            Token::T_CLOSE_TAG
+        );
         
-        $stream->expects(sprintf('%s:if', Token::T_KEYWORD));
-        $stream->expects(Token::T_OPEN_PARENTHESIS);
+        $conditions = array();
+        while (!$stream->matches($types)) {
+            $token = $stream->current();
         
-        $expression = $parser->parseExpression($stream);
+            $stream->expects(sprintf('%s:if', Token::T_IDENTIFIER), sprintf('%s:elseif', Token::T_IDENTIFIER));
+            $stream->expects(Token::T_OPEN_PARENTHESIS);
+            
+            $expression = $parser->parseExpression($stream);
+            
+            $stream->expects(Token::T_CLOSE_PARENTHESIS);
+            $stream->expects(Token::T_COLON);
+            
+            $children = $parser->parse($stream, array(
+                sprintf('%s:elseif', Token::T_IDENTIFIER), 
+                sprintf('%s:else', Token::T_IDENTIFIER), 
+                sprintf('%s:endif', Token::T_IDENTIFIER),
+                Token::T_CLOSE_TAG
+            ));
+            
+            $conditions[] = new ConditionalNode($expression, $children, $token->getLineNumber());
+        }
         
-        $stream->expects(Token::T_CLOSE_PARENTHESIS);
-        $stream->expects(Token::T_COLON);
-        
-        $children = $parser->parse($stream, array(
-            sprintf('%s:elseif', Token::T_KEYWORD),
-            sprintf('%s:else', Token::T_KEYWORD),
-            sprintf('%s:endif', Token::T_KEYWORD)
-        ));
-        
-        var_dump($children);
-        exit;
+        return $conditions;
     }
 }
