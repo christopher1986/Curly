b5c3a0bbf00615d9a660f879a0e5ad1db0dd51ac
Chris Harris <c.harris@hotmail.com>
2015-09-14 15:25:37 +0200

    This node now inherits from AbstractNode and implements the render method.

--- src/Ast/Node/IfNode.php
+++ src/Ast/Node/IfNode.php
@@ -2,7 +2,27 @@
 
 namespace Curly\Ast\Node;
 
-use Curly\Ast\Node;
+use Curly\ContextInterface;
+use Curly\Ast\AbstractNode;
 
-class IfNode extends Node
-{}
+class IfNode extends AbstractNode
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function render(ContextInterface $context)
+    {
+    
+    }
+
+    /**
+     * Returns true if the specified object is a {@link ConditionalNode} instance.
+     *
+     * @param mixed $obj the object to test.
+     * @return bool true if the specified object is a {@link ConditionalNode} instance, false otherwise.
+     */
+    protected function isNode($obj)
+    {
+        return (is_object($obj) && $obj instanceof ConditionalNode);
+    }
+}
