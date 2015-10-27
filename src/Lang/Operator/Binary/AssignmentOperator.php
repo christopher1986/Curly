18325d948357f0b78b4cc081e1993f6ed1b9770e
Chris Harris <c.harris@hotmail.com>
2015-10-27 09:50:21 +0100

    A subparser to parse an assignment operator and it's operands.

---
+++ src/Lang/Operator/Binary/AssignmentOperator.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Curly\Lang\Operator\Binary;
+
+use Curly\Ast\NodeInterface;
+use Curly\Ast\Node\Expression\Binary\AssignmentNode;
+use Curly\Lang\Operator\AbstractBinaryOperator;
+
+/** 
+ * The AssignmentOperator creates a {@link AssignmentNode} which will assign the value of 
+ * the specified right operand to the specified left operand.
+ *
+ * @author Chris Harris
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+class AssignmentOperator extends AbstractBinaryOperator
+{
+    /**
+     * {@inheritDoc}
+     */   
+    public function getSymbol()
+    {
+        return '=';
+    }
+    
+    /**
+     * {@inheritDoc}
+     */       
+    public function getPrecedence()
+    {
+        return 5;
+    }
+    
+    /**
+     * Returns the operator associativity.
+     *
+     * @return string the associativity.
+     */
+    public function getAssociativity()
+    {
+        return self::RIGHT_ASSOCIATIVE;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function createNode(NodeInterface $left, NodeInterface $right, $lineNumber = -1, $flags = 0x00)
+    {
+        return new AssignmentNode($left, $right, $lineNumber, $flags);
+    }
+}
