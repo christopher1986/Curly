568d235a53ba443825ab12117819ea91c096190b
Chris Harris <c.harris@hotmail.com>
2015-11-10 17:00:23 +0100

    Added documentation to Stream classes.

--- src/Parser/Stream/StreamInterface.php
+++ src/Parser/Stream/StreamInterface.php
@@ -1,11 +1,58 @@
 <?php
+/**
+ * Copyright (c) 2015, Chris Harris.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author     Chris Harris <c.harris@hotmail.com>
+ * @copyright  Copyright (c) 2015 Chris Harris
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ */
 
 namespace Curly\Parser\Stream;
 
 use Countable;
 
 /**
- * 
+ * A stream is very similar to a collection type in the sense that it stores elements.
+ * However streams are not traversable, which in short means that you can not iterate 
+ * through the underlying elements like you would do with an array or other traversable
+ * object.
+ *
+ * As an alternative the {@link StreamInterface::current()}, {@link StreamInterface::consume()} 
+ * and {@link StreamInterface::valid()} methods can be used to move through the stream.
+ *
+ * Streams are powerful and preferred when fine-grained control of elements is necessary.
+ * A Parser requiring a lookahead of 2 or more tokens for example may benefit from the
+ * {@link StreamInterface::peek($number)} method.
  *
  * @author Chris Harris
  * @version 1.0.0
