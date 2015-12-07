4c622c6a6240f062c09a23a108a07c2a2abde345
Chris Harris <c.harris@hotmail.com>
2015-12-07 09:16:46 +0100

    Cleaned up the methods which parse a single non-terminal symbol.

--- src/Parser.php
+++ src/Parser.php
@@ -51,7 +51,7 @@ class Parser implements ParserInterface
      * {@inheritDoc}
      */
     public function parse(TokenStream $stream, $until = null)
-    {   
+    {       
         // second argument if provided must be array.
         if (func_get_args() >= 2) {
             $until = (is_array($until)) ? $until : array_slice(func_get_args(), 1);
@@ -59,46 +59,36 @@ class Parser implements ParserInterface
     
         $this->setStream($stream);
 
-        $library = $this->getLibrary();
-        $nodes   = array();
+        $nodes = array();
         while ($stream->valid()) {
             // template tags
             $stream->consumeIf(Token::T_OPEN_TAG, Token::T_CLOSE_TAG);
-            
+
             if ($until && $stream->matches($until)) {
                 return $nodes;
             }
             
-            // print tag
+            // print tags
             if ($token = $stream->consumeIf(Token::T_OPEN_PRINT_TAG)) {
-                $expression = $this->parseExpression($stream);
-                $nodes[]    = new PrintNode(array($expression), $token->getLineNumber());
+                $nodes[] = new PrintNode($this->parseExpression($stream), $token->getLineNumber());
                 $stream->expects(Token::T_CLOSE_PRINT_TAG);
             }
             // plain text
             else if ($token = $stream->consumeIf(Token::T_TEXT)) {
                 $nodes[] = new TextNode($token->getValue(), $token->getLineNumber());
             } 
-            // statement
+            // statements
             else if ($stream->matches(Token::T_IDENTIFIER)) {
-                $token     = $stream->current();
-                $statement = $library->getStatement($token->getValue());
+                $statement = $this->getLibrary()->getStatement($stream->current()->getValue());
                 if ($statement === null) {
-                    throw new SyntaxException(sprintf('Illegal identifier "%s".', $token->getValue()), $token->getLineNumber());
+                    throw new SyntaxException(sprintf('Illegal identifier "%s".', $stream->current()->getValue()), $stream->current()->getLineNumber());
                 }
                     
                 $nodes[] = $statement->parse($this, $this->getStream());
-            // expression
+            // expressions
             } else {
                 $nodes[] = $this->parseExpression($stream);
-                if (!$stream->valid()) {
-                    throw new SyntaxException('Unexpected end of file.');
-                } else if (!$stream->matches(Token::T_SEMICOLON, Token::T_CLOSE_TAG)) {
-                    $token = $stream->current();
-                    throw new SyntaxException(sprintf('Unexpected "%s" (%s)', $token->getValue(), Token::getLiteral($token->getType())), $token->getLineNumber());
-                }
-                
-                $stream->consume();
+                $stream->expects(Token::T_SEMICOLON, Token::T_CLOSE_TAG);
             }
         }
         
@@ -110,9 +100,13 @@ class Parser implements ParserInterface
      */
     public function parseExpression(TokenStream $stream, $precedence = 0)
     {
+        if (!$stream->valid()) {
+            throw new SyntaxException('Unexpected end of file.');   
+        }
+            
         $expr = $this->parsePrimaryExpression($stream);
 
-        while (($token = $stream->current()) && $this->isBinary($token)) {           
+        while (($token = $stream->current()) && $this->isBinary($token)) {       
             $operator = $this->getBinaryOperator($token);
             if ($operator->getPrecedence() < $precedence) {
                 break;  
@@ -120,10 +114,7 @@ class Parser implements ParserInterface
             
             $stream->consume();
 
-            /*
-             * apply recursion as long as the next binary operator
-             * has a higher precedence than the current one. 
-             */
+            // do recursion while next binary operator has higher precedence than current operator. 
             $expr = $operator->createNode($expr, $this->parseExpression($stream,
 			    $operator->isRightAssociative()
                     ? $operator->getPrecedence()
@@ -139,57 +130,51 @@ class Parser implements ParserInterface
      */
     public function parsePrimaryExpression(TokenStream $stream)
     {    
-        $library = $this->getLibrary();
-        $node    = null;
-        $token   = $stream->current();
+        $node  = null;
+        $token = $stream->current();
 
-        // unary operator
-        if ($this->isUnary($token)) {
-            $stream->consume();
+        // unary operators
+        if ($this->isUnary($token) && $stream->consume()) {            
             $operator = $this->getUnaryOperator($token);            
-            $node = $operator->createNode($this->parseExpression($stream, $operator->getPrecedence()), $token->getLineNumber());
+            $node     = $operator->createNode($this->parseExpression($stream, $operator->getPrecedence()), $token->getLineNumber());
         } 
         // parenthesized expression
-        else if ($stream->consumeIf(Token::T_OPEN_PARENTHESIS)) {
-            if (!$stream->valid()) {
-                throw new SyntaxException('Unexpected end of file.');   
-            }
-            
+        else if ($stream->consumeIf(Token::T_OPEN_PARENTHESIS)) {            
             $node = $this->parseExpression($stream);
-            
-            if (!$stream->matches(Token::T_CLOSE_PARENTHESIS)) {
-                throw new SyntaxException('Expected ")"', $token->getLineNumber());
-            }
-            
-            $stream->consume();
+            $stream->expects(Token::T_CLOSE_PARENTHESIS);
         }
-        // template tag
+        // template tags
         else if ($stream->matches(Token::T_IDENTIFIER)) {
-            $tag = $library->getTag($token->getValue());
+            $tag = $this->getLibrary()->getTag($token->getValue());
             if ($tag === null) {
                 throw new SyntaxException(sprintf('Illegal identifier "%s".', $token->getValue()), $token->getLineNumber());
             }
                 
             $node = $tag->parse($this, $this->getStream());
-        // literal
-        } else if ($token) {
-            $literal = $library->getLiteral($token->getType());
-            if ($literal !== null) {
-                $node = $literal->parse($this, $stream);
-            } else if ($stream->matches(Token::T_VARIABLE)) {               
-                $node = new VariableNode($token->getValue(), $token->getLineNumber());
-                $stream->consume();
-            }
+        } 
+        // variables
+        else if ($stream->matches(Token::T_VARIABLE)) {
+            $node = new VariableNode($token->getValue(), $token->getLineNumber());
+            $stream->consume();
             
             if ($stream->matches(Token::T_OPEN_BRACKET)) {
                 $node = $this->parseArrayAccessExpression($stream, $node);
             } else if ($stream->matches(Token::T_PIPELINE)) {
                 $node = $this->parseFilterExpression($stream, $node);
             }
-        } else {
-            throw new SyntaxException(sprintf('Cannot find symbol "%s".', $token->getValue()), $token->getLineNumber());
         }
-
+        // literals
+        else if ($this->isLiteral($token)) {
+            $literal = $this->getLiteral($token);
+            $node    = $literal->parse($this, $stream);
+            
+            if ($stream->matches(Token::T_OPEN_BRACKET)) {
+                $node = $this->parseArrayAccessExpression($stream, $node);
+            } else if ($stream->matches(Token::T_PIPELINE)) {
+                $node = $this->parseFilterExpression($stream, $node);
+            }
+        }
+        
         return $node;
     }
     
@@ -247,14 +232,14 @@ class Parser implements ParserInterface
             if (!is_object($filter)) {
                 throw new SyntaxException(sprintf('Unexpected "%s" (%s)', $token->getValue(), Token::getLiteral($token->getType())), $token->getLineNumber());
             }
-                    
+
             $args = array();
             if ($stream->consumeIf(Token::T_OPEN_PARENTHESIS)) {
                 do {
                     $args[] = $this->parseExpression($stream);
                 } while ($stream->consumeIf(Token::T_COMMA));
                
-                $stream->expects(Token::T_CLOSE_PARENTHESIS);
+                $stream->expects(Token::T_CLOSE_PARENTHESIS);                
             }
             
             $node = new FilterNode($node, $filter, $args, $token->getLineNumber());
@@ -308,6 +293,28 @@ class Parser implements ParserInterface
     }
     
     /**
+     * Returns true if the specified token represents a literal.
+     *
+     * @param TokenInterface $token the token whose value will be tested.
+     * @return bool true if the specified token represents a literal, false otherwise.
+     */
+    private function isLiteral(TokenInterface $token)
+    {
+        return ($this->getLiteral($token) !== null);
+    }
+    
+    /**
+     * Returns if present a literal for the specified token.
+     *
+     * @param TokenInterface $token the token for which to find a suitable operator.
+     * @return LiteralInterface|null a literal for the specified token, or null on failure.
+     */
+    private function getLiteral(TokenInterface $token)
+    {
+        return $this->getLibrary()->getLiteral($token->getType());
+    }
+    
+    /**
      * Returns true if the specified token represents a binary operator.
      *
      * @param TokenInterface $token the token whose value will be tested.
@@ -315,18 +322,7 @@ class Parser implements ParserInterface
      */
     private function isBinary(TokenInterface $token)
     {
-        return ($this->getLibrary()->getBinaryOperator($token->getValue()) !== null);
-    }
-    
-    /**
-     * Returns true if the specified token represents a unary operator.
-     *
-     * @param TokenInterface $token the token whose value will be tested.
-     * @return bool true if the specified token represents a unary operator, false otherwise.
-     */
-    private function isUnary(TokenInterface $token)
-    {
-        return ($this->getLibrary()->getUnaryOperator($token->getValue()) !== null);
+        return ($this->getBinaryOperator($token) !== null);
     }
     
     /**
@@ -338,6 +334,17 @@ class Parser implements ParserInterface
     private function getBinaryOperator(TokenInterface $token)
     {
         return $this->getLibrary()->getBinaryOperator($token->getValue());
+    }
+    
+    /**
+     * Returns true if the specified token represents a unary operator.
+     *
+     * @param TokenInterface $token the token whose value will be tested.
+     * @return bool true if the specified token represents a unary operator, false otherwise.
+     */
+    private function isUnary(TokenInterface $token)
+    {
+        return ($this->getUnaryOperator($token) !== null);
     }
     
     /**
